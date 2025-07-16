3e6d6a7fbb651a2c59c593af5f515890d1f82b1c
Anthony Kimble <anthony.kimble@atlasscool.com>
2025-07-16 15:32:09 -0500

    the server file

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import router from './routes/index';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Mount the router for all API endpoints
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;
